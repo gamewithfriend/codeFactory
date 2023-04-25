10f21291575d0762000c66677917d1b4bff651a3
opentutorials <{jse3685545@naver.com}>
2023-04-25 21:10:34 +0900

    jth 230425-2

--- OtionSelectThree.js
+++ OtionSelectThree.js
@@ -1,14 +1,74 @@
-import React, { Component, useState } from 'react';
-import { View, Text, Button,StyleSheet,TextInput } from 'react-native';
+import React, { Component, useEffect, useState } from 'react';
+import { ScrollView, View, Text, Button,StyleSheet,TextInput,Dimensions,ActivityIndicator,Image, ImageBackground ,TouchableOpacity  } 
+from 'react-native';
 
-export default function OtionSelectThree ({navigation}) {
+const {width:SCREEN_WIDTH} = Dimensions.get('window');
+
+export default function OtionSelectThree ({ route }) {
+    const optionTrigger = false;
+    const [ok, setOptionName] = useState(1);
+    const [changeOptionValue, optionValue] = useState("true");
+    const selectGameOtion = [   
+                                {optionName:"rank",
+                                optionUrl: require("./assets/images/emblem-challenger.png")}
+                                ,
+                                {optionName:"champion",
+                                optionUrl: require("./assets/images/chmapion/Irelia_0.jpg")}
+                            ]; 
+    const [changeOptionValueTwo, optionValueTwo] = useState([]);
+                            
+    const setSelectGameOtionTwo = ()=>{
+      console.log("조건1")
+      console.log(route.params.optionOne)
+      console.log("조건2")
+      console.log(route.params.optionTwo)
+      optionValueTwo(selectGameOtion);
+      console.log(changeOptionValueTwo)   
+    };
+    
+    const optionChange = (index)=>{
+      setOptionName(index)
+      console.log(index) 
+    };
+
+     useEffect(() => {
+      setSelectGameOtionTwo();
+    },[]);                        
     return (
-        <View >
-            <View >
-                   <Text>Friend</Text>
-            </View>
-              
-        </View>
+        <View style={styles.container} >
+                <View style={styles.topContainer} >
+                    <Text style={styles.topContainerTitle}>매칭 옵션 선택</Text> 
+                </View>
+                <View style={styles.centerContainer} >
+                    <View style={styles.centerTopContainer}>
+                        <Text style={styles.centerContainerTitle}>조건2</Text>
+                    </View>
+                    <View style={styles.centerBottomContainer}>
+                    <ScrollView pagingEnabled 
+                                horizontal 
+                                showsHorizontalScrollIndicator = {false}>
+                        {changeOptionValueTwo.length === 0? (
+                            <View >
+                                <ActivityIndicator color="black" size="large"/>
+                            </View>
+                            ) : (
+                            changeOptionValueTwo.map( (info, index) =>    
+                                <View onTouchMove={text => optionChange(index)}  key={index} style={styles.contentBottom}>
+                                    <View style={styles.itemBox}>
+                                        <Text style={styles.itemBoxTitle} >{info.optionName}</Text>
+                                        <Image style={styles.backImg} source={info.optionUrl}/>       
+                                    </View>  
+                                </View>
+                            )
+                            )
+                        }         
+                    </ScrollView>
+                    </View>
+                </View> 
+                <View style={styles.bottomContainer} >
+                  <Button  title='선택하기'></Button>
+                </View>       
+            </View>  
         
       
       
@@ -16,3 +76,74 @@ export default function OtionSelectThree ({navigation}) {
   
 
 }
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+    },
+    topContainer:{       
+        flex:1,
+        alignItems:"center",
+        borderColor:"black",
+        borderStyle:"solid",
+       
+    },
+    centerContainer:{       
+        flex:6,
+        alignItems:"center",
+        borderColor:"black",
+        borderStyle:"solid",
+        
+    },
+    centerTopContainer:{       
+        flex:1,
+        alignItems:"center",
+        borderColor:"black",
+        borderStyle:"solid",
+        width:"100%",
+    },
+    centerBottomContainer:{       
+        flex:5,
+        alignItems:"center",
+        borderColor:"red",
+        borderStyle:"solid",
+        width:"100%",
+    },
+    bottomContainer:{
+        flex:3,
+        alignItems:"center",
+        borderColor:"black",
+        borderStyle:"solid",
+    },
+    topContainerTitle:{
+        marginTop: "8%",
+        fontSize: 20,
+    },
+    centerContainerTitle:{
+        marginTop: "5%",    
+        fontSize: 20,
+    },
+    contentBottom:{
+        width:SCREEN_WIDTH,
+        alignItems:"center",
+        justifyContent:"center",
+    },
+    itemBox:{
+        width:"50%",
+        height:"80%",
+        alignItems:"center",
+        borderRadius:30,
+    },
+    testWhiteText:{
+        color:"red",
+    },
+    backImg:{
+        flex:1,
+        width:'100%',
+        height:610,
+        opacity:0.7
+    },
+    itemBoxTitle:{
+      marginBottom : '5%',
+    },
+});
