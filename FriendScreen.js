43d2782f14137cf4d87cb4c32f5148f887e4180e
MoonMyungjin <105400087+MoonMyungjin@users.noreply.github.com>
2023-07-13 00:27:21 +0900

    친구창 화면 수정
    친구창 화면 수정

--- FriendScreen.js
+++ FriendScreen.js
@@ -1,8 +1,8 @@
 import React, { Component, useEffect, useState } from 'react';
-import { View, Text, Button,StyleSheet,TextInput,Dimensions,ActivityIndicator,Image, TouchableOpacity } from 'react-native';
+import { View, Text, Button,StyleSheet,TextInput,Dimensions,ActivityIndicator,Image, TouchableOpacity,Modal } from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
-import * as ImagePicker from 'expo-image-picker';
 import * as Session from './utils/session.js';
+import { useIsFocused } from '@react-navigation/native';
 import axios from 'axios';
 
 const realUrl = "3.37.211.126";
@@ -13,22 +13,15 @@ export default function FriendScreen ({navigation}) {
     let getMyNick;
     const [getFriendNum, setFriendNum] = useState(1);
     const [getStateFriendList, setStateFriendList] = useState([]);
-    const [selectedImage, setSelectedImage] = useState({
-        uri: '',
-        ext : ''
-    });
     const [userInfo, setUserInfo] = useState([]);
+    const isFocused = useIsFocused();
 
     const getFriendList = async() =>{
       let userInfo= await Session.sessionGet("sessionInfo");
-      console.log("userInfo : ", userInfo);
-      // const sessionId = userInfo.uIntgId;
-      // setUserInfo(userInfo);
-      // setMyNick(sessionId);
+      setUserInfo(userInfo);
       getMyNick = userInfo.uIntgId;
       const response = await fetch (`http://3.37.211.126:8080/friend/findFriendList.do?myNick=${getMyNick}`)
       const json = await response.json();
-      
       setStateFriendList(json.friendList)
       setFriendNum(json.friendNum);
     };
@@ -38,61 +31,13 @@ export default function FriendScreen ({navigation}) {
       const sessionId = userInfo.uIntgId;
       const response = await fetch (`http://3.37.211.126:8080/mypage/selectUserInfo.do?uIntgId=${sessionId}`)
       const json = await response.json();
-      console.log(json);
       if(json != ''){
-        setUserInfo(json.user[0]);
+          setUserInfo(json.user[0]);
       }
-    };
-
-    const openImagePickerAsync = async () => {
-      const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
-      if (permissionResult.granted === false) {
-        alert('카메라 롤에 접근 권한이 필요합니다.');
-        return;
-      }
-
-      const pickerResult = await ImagePicker.launchImageLibraryAsync();
-      console.log(pickerResult.assets[0].uri);
-      if (!pickerResult.cancelled) {
-        const lastDotIndex = pickerResult.assets[0].uri.lastIndexOf('.');
-        let fileExtension = "";
-        if (lastDotIndex !== -1) {
-          fileExtension = pickerResult.assets[0].uri.slice(lastDotIndex + 1);
-        } else {
-          fileExtension = "파일 업로드 시 확장자 없음";
-        }
-        setSelectedImage((prevState) => {
-          return {...prevState, ext : fileExtension, uri : pickerResult.assets[0].uri }
-        });
-      }
-    };
+  };
     
-    const uploadImageAsync = async () => {
-      const apiUrl = 'http://3.37.211.126:8080/mypage/changeMyImage.do';
-      const formData = new FormData();
-      
-      formData.append('image', {
-        uri: selectedImage.uri,
-        name: 'temp.'+selectedImage.ext,
-        type: 'image/*'
-      });
-
-      formData.append('data',JSON.stringify(getMyNick));
-      try{
-        
-        const response = await axios.post (apiUrl,formData,{
-            headers: {"Content-Type" : 'multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW'},
-        })
-  
-        setSelectedImage((prevState) => {
-          return {...prevState, ext : '', uri : '' }
-        });
-        const json = response.data;
-        alert("프로필파일 업로드에 성공했습니다!");
-        getMyInfo();
-      } catch(error) {
-        alert("프로필파일 업로드 실패.."+error);
-      }
+    const viewProfileDetailModal = async () => {
+      navigation.navigate('SetNickNameScreen');
     };
 
     const openChat = async (sessionInfo) => {
@@ -116,46 +61,35 @@ export default function FriendScreen ({navigation}) {
     useEffect(() => {
       getFriendList();
       getMyInfo();
-    },[]);
+    },[isFocused]);
     
     return (
         <View >
-            <View >
+            <View>
                    <Text style={styles.titleFont}>친구</Text>
-            </View>   
-            <View style={styles.profileView}>
-              <View style={styles.profileImgView}>
-              <TouchableOpacity onPress={openImagePickerAsync}>
-                {selectedImage.uri !== '' ? (
-                  <Image source={{ uri: selectedImage.uri }} style={styles.profileImg} resizeMode='contain' />
-                  ) : (
-                    userInfo.profileImgUrl !== '' ? (
-                    <Image
-                      source={{ uri: `http://3.37.211.126:8080/tomcatImg/myPage/${userInfo.profileImgUrl}`}}
-                      style={styles.profileImg}
-                      resizeMode='contain'
-                      onPress={openImagePickerAsync}
-                    />
-                  ) : (
-                    <Image resizeMode='contain' style={styles.profileImg} source={require("./assets/images/emptyProfile.jpg")}/>
-                  )
-                )}
-              </TouchableOpacity>
-                { selectedImage.uri !== '' ? (
-                    <View style={styles.profileBtnView}>
-                      <Button title="upload" onPress={uploadImageAsync}></Button>
-                    </View>
-                  ) : (
-                    <View style={styles.profileBtnView}>
-                    </View>
-                  )
-                }
-              </View>
-              <View style={styles.profileMesaageView}>
-                <Text style={styles.statusMessageFont}>{userInfo.uNickname}</Text>       
-                <Text style={styles.statusMessageFont}>테스트</Text>
-              </View>
             </View>
+            <TouchableOpacity style={styles.profileView} onPress={viewProfileDetailModal}>
+                <View style={styles.profileImgView}>
+                  {
+                    // selectedImage.uri !== '' ? (
+                    // <Image source={{ uri: selectedImage.uri }} style={styles.profileImg} resizeMode='contain' />
+                    // ) : (
+                      userInfo.profileImgUrl !== '' ? (
+                      <Image
+                        source={{ uri: `http://3.37.211.126:8080/tomcatImg/myPage/${userInfo.profileImgUrl}`}}
+                        style={styles.profileImg}
+                        resizeMode='contain'
+                      />
+                    ) : (
+                      <Image resizeMode='contain' style={styles.profileImg} source={require("./assets/images/emptyProfile.jpg")}/>
+                    )
+                    // )
+                  }
+                </View>
+                <View style={styles.profileMesaageView}>
+                  <Text style={styles.statusMessageFont}>{userInfo.unickname}</Text>       
+                </View>
+            </TouchableOpacity>
             <View style={styles.lineDesign} />
             <View style={styles.friendView}>
               <View style={styles.friendCountView}>
@@ -169,41 +103,74 @@ export default function FriendScreen ({navigation}) {
               <View style={styles.friendListView}>
                 <ScrollView pagingEnabled ={false}  
                             showsHorizontalScrollIndicator = {false}>
+                {/* {getStateFriendList.length === 0? (
+                    <View>
+                    </View>
+                  ):(
+                    <View style={styles.centerBottomContainer}>
+                      <View style={styles.itemBox}>
+                          <View style={styles.itemBoxPhoto}>
+
+                          </View>
+                          <View style={styles.itemBoxMessage}>
+                             <View style={styles.itemBoxMessageName}>
+                                <Text>소환사명</Text>
+                              </View>
+                              <View style={styles.itemBoxMessageName}>
+                                <Text>랭크</Text>
+                              </View>
+                              <View style={styles.itemBoxMessageStatus}>
+                                <Text>주챔피언</Text>
+                              </View>
+                          </View>
+                      </View>
+                    </View>
+                  )
+                } */}
                  {getStateFriendList.length === 0? (
-                            <View >
+                            <View>
                                 <ActivityIndicator color="black" size="large"/>
                             </View>
                             ) : (
-                            getStateFriendList.map( (info, index) =>    
-                              <View key={index} style={styles.centerBottomContainer} data={info.fYouId} onStartShouldSetResponder={()=>openChat(info)}>               
-                                <View style={styles.itemBox}>
-                                  <View style={styles.itemBoxPhoto}>
-                                    <Image resizeMode='contain' style={styles.profileImg}
-                                    source={require("./assets/images/emptyProfile.jpg")}/>         
-                                  </View>
-                                  <View style={styles.itemBoxMessage}>
-                                    <View style={styles.itemBoxMessageName}>
-                                      <Text>{info.fYouId}</Text>
+                              getStateFriendList.map( (info, index) =>    
+                                <View key={index} style={styles.centerBottomContainer} data={info.fYouId} onStartShouldSetResponder={()=>openChat(info)}>               
+                                  <View style={styles.itemBox}>
+                                    <View style={styles.itemBoxPhoto}>
+                                    {
+                                      info.profileImgUrl !== null ? (
+                                        <Image
+                                          source={{ uri: `http://3.37.211.126:8080/tomcatImg/myPage/${info.profileImgUrl}`}}
+                                          style={styles.profileImg}
+                                          resizeMode='contain'
+                                      />
+                                      ) : (
+                                        <Image resizeMode='contain' style={styles.profileImg}
+                                        source={require("./assets/images/emptyProfile.jpg")}/>         
+                                      )
+                                    }
                                     </View>
-                                    <View style={styles.itemBoxMessageStatus}>
-                                      <Text>블라블라블라블라블라</Text>
+                                    <View style={styles.itemBoxMessage}>
+                                      <View style={styles.itemBoxMessageName}>
+                                        <Text>{info.glSummoner}</Text>
+                                      </View>
+                                      <View style={styles.itemBoxMessageName}>
+                                        <Text>{info.glRank}</Text>
+                                      </View>
+                                      <View style={styles.itemBoxMessageStatus}>
+                                        <Text>{info.glChampion}</Text>
+                                      </View>
                                     </View>
-                                  </View>
+                                  </View> 
                                 </View> 
-                              </View> 
-                            )
+                                
+                              )
                             )
                 }                  
                 </ScrollView>
               </View>  
-            </View>   
+            </View>
         </View>
-        
-      
-      
     );
-  
-
 }
 
 const styles = StyleSheet.create({
@@ -217,24 +184,45 @@ const styles = StyleSheet.create({
         marginLeft:"10%",
       },
       profileView: {
-        width:"100%",
-        height:"12%",
+        width:"50%",
+        height:"10%",
         marginTop:"5%",
         marginLeft:"5%",
         flexDirection:"row",
       },
       profileImgView: {
-        width:"20%",
-        height:"70%",
-      },
-      profileBtnView: {
-        width:"100%",
-        height:"80%",
+        width:"50%",
+        height:"100%",
       },
       profileMesaageView: {
-        width:"75%",
+        width:"50%",
         height:"100%",
         flexDirection:"row",
+      },
+      modalContainer : {
+        // marginTop : "",
+      },
+      modalTopHeader: {
+        height:"6%",
+        backgroundColor: "red", 
+      },
+      modalTitle: {
+        fontSize:30,
+        marginLeft:"5%",
+      },
+      imgUploadBtnView: {
+        height:"7%",
+      },
+      imgUploadBtn: {
+        width:"100%",
+        height:"100%",
+        justifyContent: 'center', // 요소를 수평 축에서 오른쪽으로 정렬
+        color : "black",
+        alignItems: 'center', // 요소를 수직 축에서 아래로 정렬
+      },
+      modalTop: {
+        height:"70%",
+        backgroundColor: "green", 
       },
       profileImg: {
         width:"100%",
@@ -243,6 +231,10 @@ const styles = StyleSheet.create({
       profileOtherImg: {
         width:"100%",
         height:"100%",
+      },
+      modalBottomHeader: {
+        height:"5%",
+        // flexDirection:"row",
       },
       lineDesign:{
         height: 1, 
@@ -298,4 +290,10 @@ const styles = StyleSheet.create({
         marginLeft:"5%",
         marginTop:"10%",
       },
+      whiteSpace:{
+        height : "3%",
+      },
+      ghkrdls:{
+        backgroundColor : "red",
+      }
 });
