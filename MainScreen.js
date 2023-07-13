8245dd9509d30ae4ecdf1b38924d091fd3f8e336
opentutorials <{jse3685545@naver.com}>
2023-07-13 20:03:02 +0900

    jth 230713

--- MainScreen.js
+++ MainScreen.js
@@ -11,10 +11,13 @@ import * as Google from 'expo-auth-session/providers/google';
 import *  as Device from 'expo-device';
 import * as Network from 'expo-network';
 
+
 WebBrowser.maybeCompleteAuthSession();
 const expoClientId = '1078327323794-hqr8b6qj7lkcdtkr9snucrb5aca6lkmq.apps.googleusercontent.com';
 const iosClientId = '1078327323794-t3nm7kvjmvdg2gkac69ldninie81gkvr.apps.googleusercontent.com';
 const androidClientId = '1078327323794-scnfkq9p0i8rfqtb5rpc08vu60101q6g.apps.googleusercontent.com';
+
+
 
 const realUrl = "3.37.211.126";
 const testUrl = "192.168.105.27";
@@ -39,6 +42,8 @@ export default function MainScreen ({navigation}) {
     let reChampionList = [];
     let youserLikeCheck ="";
     let sessions = "";
+
+
 
     ////targetLike----좋아요 기능함수///////
     const targetLike = async(targetId) =>{
