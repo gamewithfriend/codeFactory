8edb8ce48df911dc6ab422ac561e4e4dfcaec500
MoonMyungjin <105400087+MoonMyungjin@users.noreply.github.com>
2023-07-16 16:29:04 +0900

    Update FriendScreen.js

--- FriendScreen.js
+++ FriendScreen.js
@@ -19,29 +19,49 @@ export default function FriendScreen ({navigation}) {
     const [getStateFriendList, setStateFriendList] = useState([]);
     const [userInfo, setUserInfo] = useState([]);
     const isFocused = useIsFocused();
+    const [modalVisible, setModalVisible] = useState(false);
+    const [getFriendInfo, setFriendInfo] = useState([]);
 
     const getFriendList = async() =>{
       let userInfo= await Session.sessionGet("sessionInfo");
       setUserInfo(userInfo);
       getMyNick = userInfo.uIntgId;
       const response = await fetch (`http://3.37.211.126:8080/friend/findFriendList.do?myNick=${getMyNick}`)
+      // const response = await fetch (`http://192.168.0.187:8080/friend/findFriendList.do?myNick=${getMyNick}`)
       const json = await response.json();
       setStateFriendList(json.friendList)
+      console.log(json.friendList)
       setFriendNum(json.friendNum);
     };
-
+    
     const getMyInfo = async() =>{
       let userInfo= await Session.sessionGet("sessionInfo");
       const sessionId = userInfo.uIntgId;
       const response = await fetch (`http://3.37.211.126:8080/mypage/selectUserInfo.do?uIntgId=${sessionId}`)
       const json = await response.json();
       if(json != ''){
-          setUserInfo(json.user[0]);
+        setUserInfo(json.user[0]);
       }
-  };
+    };
     
-    const viewProfileDetailModal = async () => {
+    const editMyProfile = async () => {
       navigation.navigate('SetNickNameScreen');
+    };
+    
+    const viewProfileModal = async (searchId) => {
+      setModalVisible(true);
+      let userInfo = await Session.sessionGet("sessionInfo");
+      const response = await fetch (`http://3.37.211.126:8080/friend/findFriendList.do?myNick=${userInfo.uIntgId}&keyWord=${searchId}`)
+      const json = await response.json();
+      console.log(json)
+      if(json != ''){
+        console.log(getFriendInfo);
+        setFriendInfo(json.friendList[0]);
+      }
+    };
+
+    const closeProfileModal = () => {
+      setModalVisible(false);
     };
 
     const openChat = async (sessionInfo) => {
@@ -75,7 +95,7 @@ export default function FriendScreen ({navigation}) {
             <View>
                    <Text style={styles.titleFont}>친구</Text>
             </View>
-            <TouchableOpacity style={styles.profileView} onPress={viewProfileDetailModal}>
+            <TouchableOpacity style={styles.profileView} onPress={editMyProfile}>
                 <View style={styles.profileImgView}>
                   {
                     // selectedImage.uri !== '' ? (
@@ -110,30 +130,27 @@ export default function FriendScreen ({navigation}) {
               <View style={styles.friendListView}>
                 <ScrollView pagingEnabled ={false}  
                             showsHorizontalScrollIndicator = {false}>
-                {/* {getStateFriendList.length === 0? (
+                {getStateFriendList.length === 0? (
                     <View>
                     </View>
                   ):(
-                    <View style={styles.centerBottomContainer}>
+                    <View style={styles.friendHeader}>
                       <View style={styles.itemBox}>
                           <View style={styles.itemBoxPhoto}>
 
                           </View>
                           <View style={styles.itemBoxMessage}>
-                             <View style={styles.itemBoxMessageName}>
+                              <View style={styles.itemBoxMessageHeaderName}>
+                                <Text>닉네임</Text>
+                              </View>
+                              <View style={styles.itemBoxMessageHeaderStatus}>
                                 <Text>소환사명</Text>
-                              </View>
-                              <View style={styles.itemBoxMessageName}>
-                                <Text>랭크</Text>
-                              </View>
-                              <View style={styles.itemBoxMessageStatus}>
-                                <Text>주챔피언</Text>
                               </View>
                           </View>
                       </View>
                     </View>
                   )
-                } */}
+                }
                  {getStateFriendList.length === 0? (
                             <View>
                                 <ActivityIndicator color="black" size="large"/>
@@ -141,7 +158,7 @@ export default function FriendScreen ({navigation}) {
                             ) : (
                               getStateFriendList.map( (info, index) =>    
                                 <View key={index} style={styles.centerBottomContainer} data={info.fYouId} onStartShouldSetResponder={()=>openChat(info)}>               
-                                  <View style={styles.itemBox}>
+                                  <TouchableOpacity style={styles.itemBox} onPress={viewProfileModal.bind(this,info.fYouId)}>
                                     <View style={styles.itemBoxPhoto}>
                                     {
                                       info.profileImgUrl !== null ? (
@@ -158,16 +175,13 @@ export default function FriendScreen ({navigation}) {
                                     </View>
                                     <View style={styles.itemBoxMessage}>
                                       <View style={styles.itemBoxMessageName}>
-                                        <Text>{info.glSummoner}</Text>
-                                      </View>
-                                      <View style={styles.itemBoxMessageName}>
-                                        <Text>{info.glRank}</Text>
+                                        <Text>{info.uNickname}</Text>
                                       </View>
                                       <View style={styles.itemBoxMessageStatus}>
-                                        <Text>{info.glChampion}</Text>
+                                        <Text>{info.glSummoner}</Text>
                                       </View>
                                     </View>
-                                  </View> 
+                                  </TouchableOpacity> 
                                 </View> 
                                 
                               )
@@ -176,6 +190,40 @@ export default function FriendScreen ({navigation}) {
                 </ScrollView>
               </View>  
             </View>
+            <Modal visible={modalVisible} animationType="slide" onRequestClose={closeProfileModal}>
+              <View style={styles.modalContainer}>
+                <View style={styles.modalTopHeader}>
+                  <Text style={styles.modalTitle}>{getFriendInfo.uNickname}</Text>
+                </View>
+                <View style={styles.modalImgView} >
+                  {
+                    getFriendInfo.profileImgUrl !== null ? (
+                      <Image
+                        source={{ uri: `http://3.37.211.126:8080/tomcatImg/myPage/${getFriendInfo.profileImgUrl}`}}
+                        style={styles.modalImg}
+                        resizeMode='contain'
+                    />
+                    ) : (
+                      <Image resizeMode='contain' style={styles.modalImg}
+                      source={require("./assets/images/emptyProfile.jpg")}/>         
+                    )
+                  }
+                </View>
+                <View style={styles.modalProfileLine}>
+                  <Text style={styles.description}> 소환사명 : </Text>
+                  <Text style={styles.content}> {getFriendInfo.glSummoner} </Text>
+                </View>
+                <View style={styles.modalProfileLine}>
+                  <Text style={styles.description}> 랭크 : </Text>
+                  <Text style={styles.content}> {getFriendInfo.glRank} </Text>
+                </View>
+                <View style={styles.modalProfileLine}>
+                  <Text style={styles.description}> 주챔피언 : </Text>
+                  <Text style={styles.content}> {getFriendInfo.glChampion} </Text>
+                </View>
+                <Button title="Close" onPress={closeProfileModal} />
+              </View>
+            </Modal>
         </View>
     );
 }
@@ -210,12 +258,32 @@ const styles = StyleSheet.create({
         // marginTop : "",
       },
       modalTopHeader: {
-        height:"6%",
-        backgroundColor: "red", 
+        height:"10%",
+        justifyContent: 'center', // 요소를 수평 축에서 오른쪽으로 정렬
+        alignItems: 'center', // 요소를 수직 축에서 아래로 정렬
       },
       modalTitle: {
-        fontSize:30,
-        marginLeft:"5%",
+        fontSize: 30,
+      },
+      modalImgView : {
+        height : "40%",
+      },
+      modalImg : {
+        height : "100%",
+        width : "100%",
+      },
+      modalProfileLine : {
+        height : "10%",
+        flexDirection:"row",
+        justifyContent: 'space-between', // 요소들을 좌우로 분배
+      },
+      description : {
+        width : "30%",
+        // marginLeft: 'auto', // 수평 좌측으로 정렬
+        // justifyContent: 'flex-end',
+      },
+      content : {
+        marginRight: 'auto', // 수평 우측으로 정렬
       },
       imgUploadBtnView: {
         height:"7%",
@@ -269,12 +337,16 @@ const styles = StyleSheet.create({
       centerBottomContainer:{       
         flex:5,
         width:"100%",
+        height:"10%",
+      },
+      friendHeader:{       
+        flex:5,
+        width:"100%",
+        height:"10%",
       },
       itemBox:{
         width:"100%",
-        height:"90%",
-        marginTop: "10%",
-        flexDirection:"row"
+        flexDirection:"row",
       },
       itemBoxPhoto:{
         width:"20%",
@@ -285,6 +357,17 @@ const styles = StyleSheet.create({
         height:"100%",
         marginLeft:"5%",
         flexDirection:"row"
+      },
+      itemBoxMessageHeaderName:{
+        width:"20%",
+        height:"100%",
+        fontSize : 18,
+      },
+      itemBoxMessageHeaderStatus:{
+        width:"80%",
+        height:"100%",
+        marginLeft:"5%",
+        fontSize : 18,
       },
       itemBoxMessageName:{
         width:"20%",
