b50b372f7f567992cbfbd81993e6bfabce5c0f06
yoZing <josephkim525@gmail.com>
2023-07-25 00:30:15 +0900

    로그인 정보 받아서 유효하지 않으면 접근차단 text 화면에 출력

--- App.js
+++ App.js
@@ -24,13 +24,15 @@ import OptionSelectFour from './OptionSelectFour';
 import OptionSelectFourDetail from './OptionSelectFourDetail';
 import SetNickNameScreen from './SetNickNameScreen';
 import TextChat from './TextChat';
-import MatchingHistoryScreen from './MatchingHistoryScreen';
+// import MatchingHistoryScreen from './MatchingHistoryScreen';
 import AlarmScreen from './AlarmScreen';
 import ChatListScreen from './ChatListScreen';
 import AccountScreen from './AccountScreen';
+import * as Session from './utils/session.js';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+let SESSION;
 
 function HomeTab(){
   return (
@@ -49,7 +51,10 @@ function HomeTab(){
   );
 }
 
-export default function App() { 
+export default function App() {
+  
+  const [isBlocked, setIsBlocked] = useState(false);
+
   Notifications.setNotificationHandler({
     handleNotification: async () => ({
       shouldShowAlert: true,
@@ -64,9 +69,94 @@ export default function App() {
         alert('알림 권한이 거부되었습니다!');
       }
     })();
-},[]); 
+},[]);
+  useEffect(() => {
+    loginCheck();
+  }, []);
+// sessionGet 메서드의 비동기적 처리를해결하기 위한 조치
+const loginCheck = async () => {
+  let session = await Session.sessionGet("sessionInfo");
+  // await sessionClear();
+
+  console.log(session);
+
+  // 세션값이 확인이 되지 않으면 구글로그인 연동 -> 구글 로그인 안에서 session setting 컨트롤
+  if (session == null || session == undefined || session == "") {
+      try {
+          promptAsync();
+          if (response?.type === 'success') { 
+              token = response.authentication.accessToken;
+              getUserInfo();
+          }
+      } catch (error) {
+          console.error(error);
+      }
+  } else {    // 세션값 확인되면 로그인 정보 최신화 후 닉네임 체크 로직 
+      checkLoginUserInfo("192.168.219.104", session);
+  }
+}
+
+const getUserInfo = async () => {
+try {
+  const response = await fetch(
+  "https://www.googleapis.com/userinfo/v2/me",
+  {
+      headers: { Authorization: `Bearer ${token}` }
+  }
+  );
+  const user = await response.json();
+  console.log(user);
+  if (user != null && user != undefined && user.verified_email ==true) {
+      userInfo.uIntgId = user.id;
+  }
+  
+  let ipAddress = await Network.getIpAddressAsync();
+  let modelName = Device.modelName;
+  
+  userInfo.uLastLoginIp = ipAddress;
+  userInfo.uLastTerminalKind = modelName;
+  
+  if (userInfo != null) {
+      checkLoginUserInfo("192.168.219.104", userInfo);
+  }
+
+} catch (error) {
+  console.log(error);
+  Alert.alert("Error!");
+}
+};
+
+const checkLoginUserInfo = async (url, data) => {
+  await fetch("http://" + url + ":8080/login/loginCheck.do", {
+                              method : "POST",
+                              headers : {
+                                  'Content-Type': 'application/json; charset=utf-8',
+                              },
+                              body : JSON.stringify(data)
+                            }).then(response => response.json()
+                             ).then(async (result) => {
+                                  // 최초로그인 및 로그인 확인이 끝났으면 session 값을 set 및 get 해준다
+                                  if (result.sessionInfo.uIntgId != null && result.sessionInfo.uIntgId != undefined && result.sessionInfo.uIntgId != "") {
+                                      let tmpSessionData = JSON.stringify(result.sessionInfo);
+
+                                      await Session.sessionSave("sessionInfo", tmpSessionData);
+
+                                      if (result.sessionInfo.uStateCd == "10601") {
+                                        setIsBlocked(false);
+                                      } else {
+                                        setIsBlocked(true);
+                                      }
+                                  }
+                             }).catch( error => {
+                                  console.error(error);
+                             }) ;
+};
+  
   return (
     <NavigationContainer>
+      {isBlocked ? (
+        <Text>접근이 차단되었습니다.</Text>
+      ) : (
       <Stack.Navigator screenOptions={{headerShown: false, gestureEnabled: true}}>            
         <Stack.Screen name="HomeTab" component={HomeTab} options={{
           headerShown:false  
@@ -104,9 +194,9 @@ export default function App() {
         <Stack.Screen name="TextChat" component={TextChat} options={{
           headerShown:false  
         }}/>
-        <Stack.Screen name="MatchingHistoryScreen" component={MatchingHistoryScreen} options={{
+        {/* <Stack.Screen name="MatchingHistoryScreen" component={MatchingHistoryScreen} options={{
           headerShown:false  
-        }}/>
+        }}/> */}
         <Stack.Screen name="AlarmScreen" component={AlarmScreen} options={{
           headerShown:false  
         }}/>
@@ -114,6 +204,7 @@ export default function App() {
           headerShown:false  
         }}/>
       </Stack.Navigator>
+      )}
     </NavigationContainer>
          
   );
